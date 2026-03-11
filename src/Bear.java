0d095dc2a76da60ee0497bf6c97e2b090729ff59
victor12706 <viro1000@stud.ek.dk>
2026-03-11 11:13:42 +0100

    Added Bear class

---
+++ src/Bear.java
@@ -0,0 +1,53 @@
+import simple.Creature;
+
+import java.util.Random;
+
+public class Bear implements Creature {
+    private String name;
+    private int health;
+    private boolean isBig;
+
+public Bear(String name) {
+    this.name = name;
+    this.health = health;
+    Random r = new Random();
+    if (r.nextInt() % 2 == 0) {
+        isBig = true;
+    } else {
+        isBig = false;
+    }
+}
+
+    @Override
+    public int attack(){
+    if (isBig){
+        return 8;
+    } else {
+        return 4;
+      }
+    }
+
+    public void takeDamage(int damage){
+    health-=damage;
+    }
+
+    public int getHealth(){
+    return health;
+    }
+
+
+
+    public String getName(){
+    return this.name;
+    }
+
+    public String meet(){
+    if (isBig) {
+        return "a giant Bear is emerging";
+    } else {
+        return "a small bear is emerging";
+    }
+    }
+
+
+}
