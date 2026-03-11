02b70626ecd350f218784de1acbb5204b4489343
fgoc <feyzullahgoctr@gmail.com>
2026-03-11 12:48:26 +0300

    Fbranch

---
+++ src/simple/Wolf.java
@@ -0,0 +1,29 @@
+package simple;
+
+public class Wolf implements Creature {
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public String meet() {
+        return "";
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+
+    }
+
+    @Override
+    public int getHealth() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
